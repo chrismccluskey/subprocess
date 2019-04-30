19d4cdd30e0601f58f0c68115967421498e581f0
Chris McCluskey <cmccluskey@digitaltrends.com>
2019-04-30 14:51:32 -0700

    Re-enable core modules

--- web-src/wp-content/mu-plugins/load-mu-plugins.php
+++ web-src/wp-content/mu-plugins/load-mu-plugins.php
@@ -20,4 +20,4 @@ if ( ! file_exists( __DIR__ . '/../../vendor/autoload.php' ) ) {
 
 require_once( __DIR__ . '/../../vendor/autoload.php' );
 
-#require_once( __DIR__ . '/sp-core/sp-core.php' );
+require_once( __DIR__ . '/sp-core/sp-core.php' );
